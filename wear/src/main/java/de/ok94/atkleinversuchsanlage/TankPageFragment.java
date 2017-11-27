d1c87e7f1720635b9f3a680f3032448a262c40ad
Oliver Kiethe <oliverkiethe@web.de>
2017-11-27 21:14:28 +0100

    Anzeige der kapazitiven Sensoren geändert

--- wear/src/main/java/de/ok94/atkleinversuchsanlage/TankPageFragment.java
+++ wear/src/main/java/de/ok94/atkleinversuchsanlage/TankPageFragment.java
@@ -1,16 +1,21 @@
 package de.ok94.atkleinversuchsanlage;
 
 import android.app.Fragment;
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TankPageFragment extends Fragment{
 
-    TextView level1View, level2View, level3View, ll1View, ll2View, ll3View, lh1View, lh2View, lh3View;
+    TextView level1View, level2View, level3View;
+    ImageView warning1View, warning2View, warning3View;
+    boolean ll1old, ll2old, ll3old, lh1old, lh2old, lh3old;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -19,12 +24,15 @@ public class TankPageFragment extends Fragment{
         level1View = (TextView) rootView.findViewById(R.id.level1);
         level2View = (TextView) rootView.findViewById(R.id.level2);
         level3View = (TextView) rootView.findViewById(R.id.level3);
-        ll1View = (TextView) rootView.findViewById(R.id.ll1);
-        ll2View = (TextView) rootView.findViewById(R.id.ll2);
-        ll3View = (TextView) rootView.findViewById(R.id.ll3);
-        lh1View = (TextView) rootView.findViewById(R.id.lh1);
-        lh2View = (TextView) rootView.findViewById(R.id.lh2);
-        lh3View = (TextView) rootView.findViewById(R.id.lh3);
+        warning1View = (ImageView) rootView.findViewById(R.id.warning1);
+        warning2View = (ImageView) rootView.findViewById(R.id.warning2);
+        warning3View = (ImageView) rootView.findViewById(R.id.warning3);
+        ll1old = true;
+        ll2old = true;
+        ll3old = true;
+        lh1old = false;
+        lh2old = false;
+        lh3old = false;
 
         return rootView;
     }
@@ -40,46 +48,52 @@ public class TankPageFragment extends Fragment{
     }
 
     public void setCapacitiveSensorStates(boolean ll1, boolean ll2, boolean ll3, boolean lh1, boolean lh2, boolean lh3) {
-        if (ll1) {
-            ll1View.setBackgroundColor(getResources().getColor(R.color.light_grey, getActivity().getTheme()));
+        if (lh1 || !ll1) {
+            warning1View.setVisibility(View.VISIBLE);
+            if (lh1 != lh1old || ll1 != ll1old) {
+                vibrate();
+            }
         }
         else {
-            ll1View.setBackgroundColor(getResources().getColor(R.color.dark_grey, getActivity().getTheme()));
+            warning1View.setVisibility(View.INVISIBLE);
+        }
+        if (lh2 || !ll2) {
+            warning2View.setVisibility(View.VISIBLE);
+            if (lh2 != lh2old || ll2 != ll2old) {
+                vibrate();
+            }
+        }
+        else {
+            warning2View.setVisibility(View.INVISIBLE);
+        }
+        if (lh3 || !ll3) {
+            warning3View.setVisibility(View.VISIBLE);
+            if (lh3 != lh3old || ll3 != ll3old) {
+                vibrate();
+            }
+        }
+        else {
+            warning3View.setVisibility(View.INVISIBLE);
         }
 
-        if (ll2) {
-            ll2View.setBackgroundColor(getResources().getColor(R.color.light_grey, getActivity().getTheme()));
-        }
-        else {
-            ll2View.setBackgroundColor(getResources().getColor(R.color.dark_grey, getActivity().getTheme()));
-        }
+        ll1old = ll1;
+        ll2old = ll2;
+        ll3old = ll3;
+        lh1old = lh1;
+        lh2old = lh2;
+        lh3old = lh3;
+    }
 
-        if (ll3) {
-            ll3View.setBackgroundColor(getResources().getColor(R.color.light_grey, getActivity().getTheme()));
+    public void vibrate() {
+        Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        long[] vibrationPattern = {0, 500, 50, 300};
+        // -1 - don't repeat
+        final int indexInPatternToRepeat = -1;
+        try {
+            vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
         }
-        else {
-            ll3View.setBackgroundColor(getResources().getColor(R.color.dark_grey, getActivity().getTheme()));
-        }
-
-        if (lh1) {
-            lh1View.setBackgroundColor(getResources().getColor(R.color.light_grey, getActivity().getTheme()));
-        }
-        else {
-            lh1View.setBackgroundColor(getResources().getColor(R.color.dark_grey, getActivity().getTheme()));
-        }
-
-        if (lh2) {
-            lh2View.setBackgroundColor(getResources().getColor(R.color.light_grey, getActivity().getTheme()));
-        }
-        else {
-            lh2View.setBackgroundColor(getResources().getColor(R.color.dark_grey, getActivity().getTheme()));
-        }
-
-        if (lh3) {
-            lh3View.setBackgroundColor(getResources().getColor(R.color.light_grey, getActivity().getTheme()));
-        }
-        else {
-            lh3View.setBackgroundColor(getResources().getColor(R.color.dark_grey, getActivity().getTheme()));
+        catch (Exception e) {
+            Log.e("VIBRATION", e.toString());
         }
     }
 }
